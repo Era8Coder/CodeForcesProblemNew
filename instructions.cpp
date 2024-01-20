f1daf7efb54592f4e42dcb78aea36fb66431df7a
Anirudh Yadav <anirudhyadavallen@gmail.com>
2024-01-20 16:43:08 +0530

    Factorization in O(n)

--- instructions.cpp
+++ instructions.cpp
@@ -3,5 +3,8 @@
 1) git clone <- some link{HTTPs Link} -> : To clone a repository on our local machine
 2) git status : displays the status of our code
 3) ls -ah : It will show all the files in it (Even hidden files also)
+4) git add <- File Name -> : To add a particular file
+5) git commit -m"Write message Here" : To commit the file with a message
+6) git push :  to push the file on GitHub
 
 */
