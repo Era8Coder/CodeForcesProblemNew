beada3cdfc6e4a001e9b1946822c86b88ad91146
Anirudh Yadav <anirudhyadavallen@gmail.com>
2024-01-20 16:36:55 +0530

    Instructions Regarding GitHUb

---
+++ instructions.cpp
@@ -0,0 +1,7 @@
+/*
+
+1) git clone <- some link{HTTPs Link} -> : To clone a repository on our local machine
+2) git status : displays the status of our code
+3) ls -ah : It will show all the files in it (Even hidden files also)
+
+*/
